f8077ff1f5e66fe60bda931ddaa666160e13c11d
Timo Wieme <1004149@hr.nl>
2026-02-15 20:02:04 +0100

    Add homepage hero component with hero image.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
+import HomepageHero from "../components/homepage/HomepageHero";
+import HeroImage from "../assets/homepage/greenhouse-hero-image.jpg";
+
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
+        <HomepageHero imageUrl={HeroImage} buttonUrl="/producten" buttonText="Producten" title="Haal de natuur in huis" subtitle="Ontdek onze unieke collectie kamerplanten, vers van eigen bodem, perfect voor jouw huis." />
     </div>
   );
 }
