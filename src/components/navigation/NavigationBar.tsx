165ac8cd7074df73b0d4c494b45cebf2b085f9c0
Timo Wieme <1004149@hr.nl>
2026-02-16 21:59:22 +0100

    Update logo and improve navigation bar styling, refactor NavbarLink to use NavLink for active state management

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,16 +1,24 @@
 import NavbarLink from "./NavbarLink";
-import Logo from "../../../public/vite.svg";
+import Logo from "../../../public/logo.svg";
 
 const NavigationBar = () => {
   return (
-    <nav className="py-4 w-full px-12">
-        <div className="flex items-center gap-4">
-            <img src={Logo} alt="Logo" className="w-10 h-10" />
-            <div className="flex gap-4">
-                <NavbarLink to="/" buttonText="Home" />
-                <NavbarLink to="/producten" buttonText="Producten" />
-            </div>
+    <nav className="py-4 w-full px-4 md:px-8">
+      <div className="flex items-center gap-4">
+          <img src={Logo} alt="Logo" className="w-12 h-12" />
+        <div className="flex gap-4">
+          <NavbarLink
+            to="/"
+            buttonText="Home"
+            activeClassName="text-primary font-bold"
+          />
+          <NavbarLink
+            to="/producten"
+            buttonText="Producten"
+            activeClassName="text-primary font-bold"
+          />
         </div>
+      </div>
     </nav>
   );
 };
